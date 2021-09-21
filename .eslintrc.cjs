c8b93c8ccc105f6dcb729ee5551a31bde05f7061
Etienne Burdet <etienne.burdet@gmail.com>
2021-09-21 09:42:15 +0200

    Fix eslint config for TS+svelte+standard

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,25 +2,26 @@
 
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  extends: ['standard-with-typescript'],
-  plugins: ['svelte3', '@typescript-eslint', 'prettier'],
-  ignorePatterns: ['*.cjs'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  parser: "@typescript-eslint/parser",
+  extends: ["standard-with-typescript"],
+  plugins: ["svelte3", "@typescript-eslint"],
+  ignorePatterns: ["*.cjs"],
+  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
   settings: {
-    'svelte3/typescript': () => require('typescript'),
+    "svelte3/typescript": () => require("typescript"),
   },
   parserOptions: {
-    sourceType: 'module',
+    sourceType: "module",
     ecmaVersion: 2019,
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
+    extraFileExtensions: [".svelte"]
   },
   env: {
     browser: true,
     es2017: true,
     node: true,
   },
-  rules: {
-    '@typescript-eslint/strict-boolean-expressions': 'off'
+  rules: {
+    "no-multiple-empty-lines": ['error', { max: 2, maxBOF: 2, maxEOF: 0 }] //Bug with Svelte
   }
 };
