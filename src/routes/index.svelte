0e6018c6a746fdd56963e894c5e1dee8b68723bf
Etienne Burdet <etienne.burdet@gmail.com>
2021-09-22 21:55:53 +0200

    Add types for calls

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,35 @@
 <script context="module" lang="ts">
-import type { LoadOutput } from '@sveltejs/kit'
-
-
+  type Family = {
+    id: string
+    fields: {
+      nom: string
+      dossier: string
+      membres?: string[]
+      nomsMembres?: string[]
+      adresse?: string
+    }
+  }
+  type Families = Family[]
 /**
    * @type {import'@sveltejs/kit').Load}
    */
-export async function load ({ page, fetch, session, context }) {
-  const res = await fetch('/airtable/familles')
+export async function load ({ page, fetch, session, context }): Promise<{ props: { families: Families } }|undefined> {
+    const res = await fetch('/airtable/familles')
 
-  const { records: families } = await res.json()
+    const { records: families } = await res.json()
 
-  if (res.ok) {
-    return {
-      props: {
-        families
+    if (res.ok) {
+      return {
+        props: {
+          families
+        }
       }
     }
-  }
 }
 </script>
 
 <script lang="ts">
-  export let families
+  export let families: Families
 </script>
 
 {#await families}
@@ -31,11 +39,11 @@ export async function load ({ page, fetch, session, context }) {
     <h2>{family.fields.nom}</h2>
     {JSON.stringify(family.fields)}
     <ul>
-      {#if family.fields.membres}
+      {#if family.fields.membres && family.fields.nomsMembres}
         {#each family.fields.membres as membre, index (membre)}
           <li>
             <a href="/membres/{membre}"
-              >{family.fields.noms_membres[index]}</a
+              >{family.fields.nomsMembres[index]}</a
             >
           </li>
         {/each}
--- src/routes/membres/[id].svelte
+++ src/routes/membres/[id].svelte
@@ -1,11 +1,16 @@
 <script context="module" lang="ts">
 import type { LoadInput } from '@sveltejs/kit'
-
-
+type Membre = {
+  nom: string
+  email?: string
+  photo?: {
+    url: string
+  }
+}
 /**
    * @type {import'@sveltejs/kit').Load}
    */
-export async function load ({ page, fetch, session, context }: LoadInput) {
+export async function load ({ page, fetch, session, context }: LoadInput): Promise<{ props: { membre: Membre } }|undefined> {
   const { id } = page.params
   const res = await fetch(`/airtable/membres/${id}`)
   const { fields: membre } = await res.json()
@@ -21,15 +26,17 @@ export async function load ({ page, fetch, session, context }: LoadInput) {
 </script>
 
 <script lang="ts">
-  export let membre
+  export let membre: Membre
 </script>
 
 {#await membre}
   Loading records
 {:then}
   <h2>{membre.nom}</h2>
-  {JSON.stringify(membre.Photo)}
-  <img src={membre.Photo[0].url} alt="">
+  {#if membre.photo}
+    <img src={membre.photo[0].url} alt="">
+  {/if}
+
 {:catch}
   No records
 {/await}
